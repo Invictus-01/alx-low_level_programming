27d386e60185c488eaafb6530402920e366d1aaf
Invictus-01 <ijomajoseph@gmail.com>
2022-09-08 09:16:42 -0700

    Adding C files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * initialise a string - adding the values
+ * print the string using puts
+ * Return: 0 once successful
+ */
+int main(void)
+{
+	char str1[] = "Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * display output to stdout using printf
+ * Return: 0 once successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
