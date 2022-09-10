593fc39cd4d5b1447f41683e2b6b2380c985ac9e
Invictus-01 <ijomajoseph@gmail.com>
2022-09-10 03:18:21 +0100

    Adding C files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 once successful
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+	printf("%d", i);
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
