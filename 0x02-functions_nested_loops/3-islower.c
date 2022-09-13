82c50df8fa31b752264a489002503ddbdad052df
Invictus-01 <ijomajoseph@gmail.com>
2022-09-13 18:42:19 +0100

    Adding C files

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _islower - main function
+* @c: The given character
+* Return: 1 if c is lowercase
+* Will  return 0 if otherwise
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
