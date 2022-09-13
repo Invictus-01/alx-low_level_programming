f6d6365aef792c07132d4735d29eccff730199e8
Invictus-01 <ijomajoseph@gmail.com>
2022-09-13 17:54:22 +0100

    Adding C file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - the main function
+* Return: 0 once successful
+*/
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char alph;
+
+	while (i <= 10)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph);
+	}
+	i++;
+	_putchar('\n');
+}
