c615026745d7eb502be874ce3602c7c543b43619
Invictus-01 <ijomajoseph@gmail.com>
2022-09-13 20:42:31 +0100

    Adding C files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _abs - main function
+* @n: The given variable
+* Return: the absolute value of n
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
