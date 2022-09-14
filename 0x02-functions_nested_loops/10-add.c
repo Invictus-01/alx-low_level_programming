c3bc6583f808aeb134394af932e267b74e41e82a
Invictus-01 <ijomajoseph@gmail.com>
2022-09-14 01:16:20 +0100

    Adding C files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - main function that adds two integers
+* @a: given integer to be added to b
+* @b: given integer to be added to a
+* Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
