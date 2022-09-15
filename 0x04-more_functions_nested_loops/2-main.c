a56b46d5630e4204cff5c305a28284f08de80602
Invictus-01 <ijomajoseph@gmail.com>
2022-09-15 11:04:20 +0100

    Adding C files

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - checks the code
+*
+* Return: 0 once successful
+*/
+int main(void)
+{
+	printf("%d\n", mul(10, 10));
+	printf("%d\n", mul(50, 2));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - main function
+* @a: The first integer that multiplies b
+* @b: The second integer that multiplies a
+* Return: the result of the multiplication
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
