cf8204a7ed905ba223db1cce42ddb5c903df2af6
Invictus-01 <ijomajoseph@gmail.com>
2022-09-15 16:41:21 +0100

    Adding C files

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* main - checks the code
+* Return: 0 once successful
+*/
+int main(void)
+{
+	print_triangle(4);
+	print_triangle(12);
+	print_triangle(0);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main - entry point
+* print numbers from 1 to 100 with conditions
+* Return: 0 once successful
+*/
+int main(void)
+{
+	int a;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 5 == 0) && (a % 3 == 0))
+		{
+			printf("%s ", fb);
+		}
+		else if ((a % 5) == 0)
+		{
+			printf("%s ", b);
+		}
+		else if ((a % 3) == 0)
+		{
+			printf("%s ", f);
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
