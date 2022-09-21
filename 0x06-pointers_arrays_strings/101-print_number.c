5158818abd7b736094ebf80238bb0cc6ee0d8783
Invictus-01 <ijomajoseph@gmail.com>
2022-09-21 18:40:46 +0100

    Adding for checks after testing files

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+* print_number - function that prints an integer
+* @n: the given integer
+*/
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	}
+	else
+	{
+		m = n;
+	}
+	if (m /10)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
+}
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
