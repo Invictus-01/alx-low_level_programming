8cc53194769de18992e1c407ec33208dc0d4dadf
Invictus-01 <ijomajoseph@gmail.com>
2022-09-21 20:58:15 +0100

    Making amendments

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_number - thevfunction that prints an integer;
+ * print_number - the function that prints an integer;
  * @n: the given integer to be printed;
  */
 void print_number(int n)
