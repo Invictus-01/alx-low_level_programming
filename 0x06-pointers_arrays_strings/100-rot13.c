ebced94d33c09b5da3c2fd9e60696612f56411fa
Invictus-01 <ijomajoseph@gmail.com>
2022-09-21 20:53:30 +0100

    Making amendments

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,21 +1,27 @@
 #include "main.h"
 /**
- * rot13 - rotates first 13 characters with the second 13 in the alphabet
- * @c: the pointer variable holding the string
- * Return: the rotated string 'c'
- */
-char *rot13(char *c)
+* rot13 - encodes a string of 13 characters
+* @s: the given string to be encoded
+* Return: the encoded string 's'
+*/
+char *rot13(char *s)
 {
-	int a;
-	char str1[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char str2[] = "nopqrstuvwxyzabcdefghijklm";
+	int i, m;
 
-	for (a = 0; c[a] != '\0'; a++)
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((c[a] > 64 && c[a] < 91) || (c[a] > 96 && c[a] < 123))
+		for (m = 0; c[m] != '\0'; m++)
 		{
-			c[a] = ((c[a] - 65) > 25) ? str2[c[a] - 97] : str1[c[a] - 65];
+			if (s[i] == c[m])
+			{
+				s[i] = d[m];
+				break;
+			}
 		}
 	}
-	return (c);
+
+	return (s);
 }
