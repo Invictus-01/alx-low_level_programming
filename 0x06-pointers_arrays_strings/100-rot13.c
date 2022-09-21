f60f3271aee2194f0fc439b90cd419db80b93a84
Invictus-01 <ijomajoseph@gmail.com>
2022-09-21 20:34:43 +0100

    Making amendments

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,14 +7,14 @@
 char *rot13(char *c)
 {
 	int a;
-	char str1[] = "nopqrstuvwxyzabcdefghijklm";
-	char str2[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char str1[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char str2[] = "nopqrstuvwxyzabcdefghijklm";
 
 	for (a = 0; c[a] != '\0'; a++)
 	{
 		if ((c[a] > 64 && c[a] < 91) || (c[a] > 96 && c[a] < 123))
 		{
-			c[a] = ((c[a] - 65) > 25) ? str1[c[a] - 97] : str2[c[a] - 65];
+			c[a] = ((c[a] - 65) > 25) ? str2[c[a] - 97] : str1[c[a] - 65];
 		}
 	}
 	return (c);
