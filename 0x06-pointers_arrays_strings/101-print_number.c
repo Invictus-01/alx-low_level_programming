2e1c953803d752d765134c63c94de24f4f761548
Invictus-01 <ijomajoseph@gmail.com>
2022-09-21 20:42:35 +0100

    Making amendments

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,28 +16,7 @@ void print_number(int n)
 	{
 		m = n;
 	}
-	if (m /10)
+	if (m / 10)
 		print_number(m / 10);
 	_putchar((m % 10) + '0');
-}
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
 }
