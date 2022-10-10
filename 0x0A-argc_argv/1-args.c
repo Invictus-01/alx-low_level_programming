e7f7c0a76bf7f7e2704d146a2fc9db4a249e9387
Invictus-01 <ijomajoseph@gmail.com>
2022-10-10 10:46:46 +0100

    Adding files for checks

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - prints all arguments it receives
+ * main - print the number of arguments passed to the program
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 once successful
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
